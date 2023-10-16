093916494c10787e170f08683fd1c49a8cd5a63b
joaaovitoorsantos <joaaovitoorsantos@gmail.com>
2023-10-16 11:37:55 -0300

    done

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -3,46 +3,21 @@ import Slider from "react-slick";
 
 export default function Carousel({ images }) {
   const settings = {
+    className: 'center',
     dots: true,
     infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
   };
 
   return (
-      <Slider {...settings}>
-        {images.map((img, index) => (
-          <div key={index} className="w-full">
-            <img className="mx-auto w-full max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-3xl" src={img} alt={`Carrossel imagem ${index}`}/>
-          </div>
-        ))}
-      </Slider>
+    <Slider {...settings}>
+      {images.map((img, index) => (
+        <img src={img} alt={`Carrossel imagem ${index}`} />
+      ))}
+    </Slider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import FourthSection from '@components/fourthSection'
 import FifthSection from '@components/fifthSection'
 import LastSection from '@components/lastSection'
 import Carousel from '@components/Carousel';
+import Slider from 'react-slick'
 
 export default function Home() {
 
@@ -18,16 +19,45 @@ export default function Home() {
     // "https://i.imgur.com/7Osfxv1.png"
   ];
 
+  const settings = {
+    focusOnSelect: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    responsive: [
+      {
+        breakpoint: 640, // a partir de 640px
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 1024, // a partir de 1024px
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
+  };
+
   return (
     <>
       <Header />
       <SecondSection />
       <FourthSection />
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        <div className="w-full sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2">
-          <Carousel images={images} />
+      <div className='bg-white'>
+        <div className='w-full md:w-4/5 lg:w-3/4 xl:w-2/3 mx-auto px-8 sm:px-0 min-h-[300px]'>
+          <Slider {...settings}>
+            {images.map((img, index) => (
+              <div key={index} className="p-2">
+                <img className="mx-auto w-full h-full object-contain" src={img} alt={`Carrossel imagem ${index}`} />
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
+
 
       <LastSection />
     </>
