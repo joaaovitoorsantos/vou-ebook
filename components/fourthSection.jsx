057ad724d2eb89bdbede0c39c933fd8a70064e58
joaaovitoorsantos <joaaovitoorsantos@gmail.com>
2023-10-18 12:33:09 -0300

    done

--- components/fourthSection.jsx
+++ components/fourthSection.jsx
@@ -3,7 +3,7 @@ export default function FourthSection() {
         <div className="bg-gray-100 text-gray-900 py-20 px-8 flex flex-col md:flex-row items-center justify-center space-y-12 md:space-y-0">
 
             <div className="flex-1 relative md:ml-10">
-                <img src="/gif.gif" alt="Cidade à noite" className="rounded-lg shadow-xl w-full"/>
+                <img src="https://media.discordapp.net/attachments/1164222962455564390/1164224019101397013/gif_2.gif?ex=65426f1c&is=652ffa1c&hm=fb262fa3c1ac6437478a9a0ad4ae73db8b0fe079a69755a834115bc218872961&=&width=994&height=559" alt="Cidade à noite" className="rounded-lg shadow-xl w-full"/>
             </div>
             <div className="flex-1 space-y-6 md:ml-10 p-8 bg-white rounded-xl shadow-xl">
                 <h2 className="text-3xl font-bold mb-4">A Vou! já foi selecionada para os maiores eventos de inovação da América Latina</h2>
